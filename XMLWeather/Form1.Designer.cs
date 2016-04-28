7297c560e103ef634cb7e74e9d47422b7c200a39
Zevshoa <zshoag@gmail.com>
2016-04-28 10:02:41 -0400

    Got today working but having problems with forecast

--- XMLWeather/Form1.Designer.cs
+++ XMLWeather/Form1.Designer.cs
@@ -29,37 +29,149 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.cityOutput = new System.Windows.Forms.Label();
+            this.dayComboBox = new System.Windows.Forms.ComboBox();
+            this.locationLabel = new System.Windows.Forms.Label();
+            this.windSpeedLabel = new System.Windows.Forms.Label();
+            this.currentTempLabel = new System.Windows.Forms.Label();
+            this.windDirectionLabel = new System.Windows.Forms.Label();
+            this.highLowTempLabel = new System.Windows.Forms.Label();
+            this.windLabel = new System.Windows.Forms.Label();
+            this.conditionBox = new System.Windows.Forms.PictureBox();
+            this.popBox = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.conditionBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // cityOutput
+            // dayComboBox
             // 
-            this.cityOutput.AutoSize = true;
-            this.cityOutput.Location = new System.Drawing.Point(61, 120);
-            this.cityOutput.Name = "cityOutput";
-            this.cityOutput.Size = new System.Drawing.Size(35, 13);
-            this.cityOutput.TabIndex = 0;
-            this.cityOutput.Text = "label1";
+            this.dayComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dayComboBox.FormattingEnabled = true;
+            this.dayComboBox.Items.AddRange(new object[] {
+            "Today",
+            "Tomorrow",
+            "Tomorrow\'s Tomorrow",
+            "The Day After That"});
+            this.dayComboBox.Location = new System.Drawing.Point(31, 35);
+            this.dayComboBox.Name = "dayComboBox";
+            this.dayComboBox.Size = new System.Drawing.Size(211, 28);
+            this.dayComboBox.TabIndex = 9;
+            this.dayComboBox.SelectedIndexChanged += new System.EventHandler(this.dayComboBox_SelectedIndexChanged);
+            // 
+            // locationLabel
+            // 
+            this.locationLabel.AutoSize = true;
+            this.locationLabel.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationLabel.Location = new System.Drawing.Point(722, 413);
+            this.locationLabel.Name = "locationLabel";
+            this.locationLabel.Size = new System.Drawing.Size(72, 48);
+            this.locationLabel.TabIndex = 10;
+            this.locationLabel.Text = "city";
+            // 
+            // windSpeedLabel
+            // 
+            this.windSpeedLabel.AutoSize = true;
+            this.windSpeedLabel.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.windSpeedLabel.Location = new System.Drawing.Point(74, 336);
+            this.windSpeedLabel.Name = "windSpeedLabel";
+            this.windSpeedLabel.Size = new System.Drawing.Size(191, 48);
+            this.windSpeedLabel.TabIndex = 11;
+            this.windSpeedLabel.Text = "windSpeed";
+            // 
+            // currentTempLabel
+            // 
+            this.currentTempLabel.AutoSize = true;
+            this.currentTempLabel.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentTempLabel.Location = new System.Drawing.Point(668, 84);
+            this.currentTempLabel.Name = "currentTempLabel";
+            this.currentTempLabel.Size = new System.Drawing.Size(217, 48);
+            this.currentTempLabel.TabIndex = 12;
+            this.currentTempLabel.Text = "currentTemp";
+            // 
+            // windDirectionLabel
+            // 
+            this.windDirectionLabel.AutoSize = true;
+            this.windDirectionLabel.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.windDirectionLabel.Location = new System.Drawing.Point(74, 256);
+            this.windDirectionLabel.Name = "windDirectionLabel";
+            this.windDirectionLabel.Size = new System.Drawing.Size(233, 48);
+            this.windDirectionLabel.TabIndex = 13;
+            this.windDirectionLabel.Text = "windDirection";
+            // 
+            // highLowTempLabel
+            // 
+            this.highLowTempLabel.AutoSize = true;
+            this.highLowTempLabel.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highLowTempLabel.Location = new System.Drawing.Point(381, 84);
+            this.highLowTempLabel.Name = "highLowTempLabel";
+            this.highLowTempLabel.Size = new System.Drawing.Size(177, 48);
+            this.highLowTempLabel.TabIndex = 15;
+            this.highLowTempLabel.Text = "highTemp";
+            // 
+            // windLabel
+            // 
+            this.windLabel.AutoSize = true;
+            this.windLabel.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.windLabel.Location = new System.Drawing.Point(74, 189);
+            this.windLabel.Name = "windLabel";
+            this.windLabel.Size = new System.Drawing.Size(110, 48);
+            this.windLabel.TabIndex = 17;
+            this.windLabel.Text = "Wind:";
+            // 
+            // conditionBox
+            // 
+            this.conditionBox.InitialImage = global::XMLWeather.Properties.Resources.sun;
+            this.conditionBox.Location = new System.Drawing.Point(662, 174);
+            this.conditionBox.Name = "conditionBox";
+            this.conditionBox.Size = new System.Drawing.Size(200, 200);
+            this.conditionBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.conditionBox.TabIndex = 16;
+            this.conditionBox.TabStop = false;
+            // 
+            // popBox
+            // 
+            this.popBox.AutoSize = true;
+            this.popBox.Font = new System.Drawing.Font("Microsoft MHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.popBox.Location = new System.Drawing.Point(184, 445);
+            this.popBox.Name = "popBox";
+            this.popBox.Size = new System.Drawing.Size(81, 48);
+            this.popBox.TabIndex = 18;
+            this.popBox.Text = "PoP";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(244, 269);
-            this.Controls.Add(this.cityOutput);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(927, 558);
+            this.Controls.Add(this.popBox);
+            this.Controls.Add(this.windLabel);
+            this.Controls.Add(this.conditionBox);
+            this.Controls.Add(this.highLowTempLabel);
+            this.Controls.Add(this.windDirectionLabel);
+            this.Controls.Add(this.currentTempLabel);
+            this.Controls.Add(this.windSpeedLabel);
+            this.Controls.Add(this.locationLabel);
+            this.Controls.Add(this.dayComboBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Central Weather";
+            ((System.ComponentModel.ISupportInitialize)(this.conditionBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label cityOutput;
-
+        private System.Windows.Forms.ComboBox dayComboBox;
+        private System.Windows.Forms.Label locationLabel;
+        private System.Windows.Forms.Label windSpeedLabel;
+        private System.Windows.Forms.Label currentTempLabel;
+        private System.Windows.Forms.Label windDirectionLabel;
+        private System.Windows.Forms.Label highLowTempLabel;
+        private System.Windows.Forms.PictureBox conditionBox;
+        private System.Windows.Forms.Label windLabel;
+        private System.Windows.Forms.Label popBox;
     }
 }
 
--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -11,10 +11,19 @@ using System.Xml;
 
 namespace XMLWeather
 {
+    
     public partial class Form1 : Form
     {
+        public static string whatDay;
+        string currrentCity, currentDirection, currentTemp, currentHighTemp, currentLowTemp, currentWindSpeed;
+        //string tomorowCity, tomorowDirection, tomorowTemp, tomorowHighTemp, tomorowLowTemp, tomorowWindSpeed;
+        //string tomorowTomorowCity, tomorowTomorowDirection, tomorowTomorowTemp, tomorowTomorowHighTemp, tomorowTomorowLowTemp, tomorowTomorowWindSpeed;
+        int currentConditions, tomorowConditions, tomorowTomorowConditions;
+        Day ds;
+
         public Form1()
         {
+           
             InitializeComponent();
 
             // get information about current and forecast weather from the internet
@@ -33,36 +42,204 @@ namespace XMLWeather
 
             string currentFile = "http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
             string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
-            
+
             client.DownloadFile(currentFile, "WeatherData.xml");
             client.DownloadFile(forecastFile, "WeatherData7Day.xml");
         }
-
         private void ExtractCurrent()
         {
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load("WeatherData.xml");
+
+            ////create a node variable to represent the parent element
+            //XmlNode parent;
+            //parent = doc.DocumentElement;
+
+            ////check each child of the parent element
+            //foreach (XmlNode child in parent.ChildNodes)
+            //{
+            //    // TODO if the "city" element is found display the value of it's "name" attribute
+            //    if (child.Name == "city")
+            //    {
+            //       currrentCity = child.Attributes["name"].Value;
+            //    }
+            //    if (child.Name == "temperature")
+            //    {
+            //        currentHighTemp = child.Attributes["max"].Value;
+            //        currentLowTemp = child.Attributes["min"].Value;
+            //        currentTemp = child.Attributes["value"].Value;
+            //    }
+            //    if (child.Name == "wind")
+            //    {
+            //        foreach (XmlNode grandChild in child.ChildNodes)
+            //        {
+            //            if (grandChild.Name == "speed")
+            //            {
+            //                currentWindSpeed = grandChild.Attributes["name"].Value;
+            //            }
+            //            if (grandChild.Name == "direction")
+            //            {
+            //                currentDirection = grandChild.Attributes["name"].Value;
+            //            }
+            //        }
+            //    }
+            //    if(child.Name == "weather")
+            //    {
+            //        currentConditions = Convert.ToInt32(child.Attributes["number"].Value);
+            //    }
+            //}
+        }
+
+        private void ExtractForecast()
+        {
             XmlDocument doc = new XmlDocument();
-            doc.Load("WeatherData.xml");
+            doc.Load("WeatherData7Day.xml");
 
             //create a node variable to represent the parent element
             XmlNode parent;
             parent = doc.DocumentElement;
-
+            int day = 1;
             //check each child of the parent element
             foreach (XmlNode child in parent.ChildNodes)
             {
-                // TODO if the "city" element is found display the value of it's "name" attribute
+                if (child.Name == "forecast")
+                {
+                    foreach (XmlNode grandChild in child.ChildNodes)
+                    {
+                        foreach (XmlNode greatGrandChild in grandChild.ChildNodes)
+                        {
+                            if(greatGrandChild.Name == "symbol")
+                            {
+                                //switch (day)
+                                //{
+                                //    case 1:
+                                //        break;
+                                //    case 2:
+                                //        tomorowConditions = Convert.ToInt32(grandChild.Attributes["number"].Value);
+                                //        break;
+                                //    case 3:
+                                //        tomorowTomorowConditions = Convert.ToInt32(grandChild.Attributes["number"].Value);
+                                //        break;
+                                //    case 4:
+                                //        break;
+                                //    default:
+                                //        break;
+                                //}
+                            }
+                            if (greatGrandChild.Name == "temperature")
+                            {
+                                switch(day)
+                                {
+                                    case 1:
+                                        //maxForeOut.Text = greatGrandChild.Attributes["max"].Value;
+                                        //minForeOut.Text = greatGrandChild.Attributes["min"].Value;
+                                        break;
+                                    case 2:
+                                        //tomorowTemp = greatGrandChild.Attributes["day"].Value;
+                                        //tomorowHighTemp = greatGrandChild.Attributes["max"].Value;
+                                        //tomorowLowTemp = greatGrandChild.Attributes["min"].Value;
+                                        break;
+                                    default:
+                                        break;
+                                } 
+                            }
+                            if(greatGrandChild.Name == "clouds")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        //cloudLabel1.Text = greatGrandChild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    case 2:
+                                        //cloudLabel2.Text = greatGrandChild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                           
+                        }
+                    }
+                }
             }
         }
-              
-        private void ExtractForecast()
+        public void conditionPictureChooser()
         {
-                     
-        }
+            if (ds.conditions  >= 200 && ds.conditions <= 299)
+            {
+                conditionBox.Image = Properties.Resources.storm;
+            }
+            else if (ds.conditions >= 300 && ds.conditions <= 532)
+            {
+                conditionBox.Image = Properties.Resources.storm;
+            }
+            else if (ds.conditions >= 600 && ds.conditions  <= 632)
+            {
+                conditionBox.Image = Properties.Resources.snow;
+            }
+            else if (ds.conditions == 781)
+            {
+                conditionBox.Image = Properties.Resources.Tornado;
+            }
+            else if (ds.conditions == 800)
+            {
+                conditionBox.Image = Properties.Resources.sun;
+            }
+            else if (ds.conditions >= 801 && ds.conditions <= 804)
+            {
+                conditionBox.Image = Properties.Resources.cloud;
+            }
+            else if (ds.conditions >= 951 && ds.conditions <= 962)
+            {
+                conditionBox.Image = Properties.Resources.cloud;
+            }
+            else
+            {
+                conditionBox.Image = null;
+            }
 
+
+
+
+
+
+        }
 
         private void MakePictureParent()
         {
             
+        }
+
+        private void dayComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+            if (dayComboBox.Text == "Today")
+            {
+                ds = new Day(currrentCity, currentDirection, currentTemp, currentHighTemp, currentLowTemp, currentWindSpeed, currentConditions);
+                whatDay = "day1";
+                ds.weatherChooser();
+                highLowTempLabel.Text =ds.highTemp+"/"+ds.lowTemp;
+                currentTempLabel.Text = ds.currentTemp+ "°C";
+                windDirectionLabel.Text = ds.windDirection;
+                windSpeedLabel.Text = ds.windSpeed;
+                locationLabel.Text = ds.city;
+                conditionPictureChooser(); 
+            }
+            else if(dayComboBox.Text == "Tomorrow")
+            {
+                ds = new Day(currrentCity, currentDirection, currentTemp, currentHighTemp, currentLowTemp, currentWindSpeed, currentConditions);
+                whatDay = "day2";
+                ds.weatherChooser();
+                highLowTempLabel.Text = ds.highTemp + "/" + ds.lowTemp;
+                currentTempLabel.Text = ds.currentTemp + "°C";
+                windDirectionLabel.Text = ds.windDirection;
+                windSpeedLabel.Text = ds.windSpeed;
+                locationLabel.Text = ds.city;
+                conditionPictureChooser();
+
+            }
         }
 
     }
